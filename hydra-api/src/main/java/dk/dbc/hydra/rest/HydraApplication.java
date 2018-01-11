e49545cd563eba1b2ab6ab73f0aee60c4c4f1790
Anders Thrane Michelsen <atm@dbc.dk>
2018-01-11 13:27:08 +0100

    Add initial source files

---
+++ hydra-api/src/main/java/dk/dbc/hydra/rest/HydraApplication.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
+ *  See license text at https://opensource.dbc.dk/licenses/gpl-3.0
+ */
+
+package dk.dbc.hydra.rest;
+
+import dk.dbc.hydra.HydraAPI;
+import dk.dbc.hydra.LibraryAPI;
+import dk.dbc.hydra.QueueAPI;
+import dk.dbc.hydra.StatisticsAPI;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This class defines the other classes that make up this JAX-RS application
+ * by having the getClasses method return a specific set of resources.
+ */
+@ApplicationPath("/api")
+public class HydraApplication extends Application {
+    private static final Set<Class<?>> classes = new HashSet<>(Arrays.asList(QueueAPI.class, LibraryAPI.class, HydraAPI.class, StatisticsAPI.class));
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return classes;
+    }
+}
