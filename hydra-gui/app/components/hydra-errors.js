bd474af469ac03a2815e290a31e66284846a9fc2
Jan Bauer Nielsen <jbn@dbc.dk>
2020-01-28 12:08:09 +0100

    Present errors grouped by workers
    Auditors: atm, jbr, hwha

--- hydra-gui/app/components/hydra-errors.js
+++ hydra-gui/app/components/hydra-errors.js
@@ -4,18 +4,63 @@
  */
 
 import React from 'react';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import {Button} from 'react-bootstrap';
+import superagent from 'superagent';
 
 class HydraErrors extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            errorsByWorker: null,
 
+            loadingErrorsByWorker: false
         };
+
+        this.fetchErrorsByWorkers = this.fetchErrorsByWorkers.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchErrorsByWorkers();
+    }
+
+    fetchErrorsByWorkers() {
+        this.setState({loadingErrorsByWorker: true});
+        superagent.get('/api/errors/byWorker').end((err, res) => {
+            if (err) {
+                alert("FEJL!\n\nDer opstod fejl under kald til /api/errors/byWorker:\n" + err)
+            } else if (res.body === null) {
+                alert('FEJL!\n\nDer kom tomt svar tilbage fra api/errors/byWorker');
+            } else {
+                this.setState({
+                    errorsByWorker: res.body,
+                    loadingErrorsByWorker: false
+                });
+            }
+        });
     }
 
     render() {
         return (
             <div>
+                <h2>Fejloversigt - workers</h2>
+                <BootstrapTable
+                    data={this.state.errorsByWorker}
+                    striped={true}
+                    options={{noDataText: 'Der blev ikke fundet nogen rækker'}}
+                    bordered={false}>
+                    <TableHeaderColumn dataField='worker' isKey>Worker</TableHeaderColumn>
+                    <TableHeaderColumn dataField='count'>Antal</TableHeaderColumn>
+                    <TableHeaderColumn dataField='date'>Ajourdato</TableHeaderColumn>
+                </BootstrapTable>
+                <br/>
+                <Button
+                    onClick={this.fetchErrorsByWorkers}
+                    type='submit'
+                    className='btn btn-success'
+                    disabled={this.state.loadingErrorsByWorker}>
+                    Genindlæs
+                </Button>
             </div>
         )
     }
