26b0fafef16242d94fce78d3a68c93019168050e
Jan Bauer Nielsen <jbn@dbc.dk>
2020-01-29 13:56:53 +0100

    Simplify API methods
    Let the framework handle the response generation.
    
    Auditors: atm, jbr, hwha

--- hydra-api/src/main/java/dk/dbc/hydra/ErrorsAPI.java
+++ hydra-api/src/main/java/dk/dbc/hydra/ErrorsAPI.java
@@ -5,8 +5,6 @@
 
 package dk.dbc.hydra;
 
-import dk.dbc.commons.jsonb.JSONBContext;
-import dk.dbc.commons.jsonb.JSONBException;
 import dk.dbc.hydra.common.ApplicationConstants;
 import dk.dbc.hydra.dao.RawRepoConnector;
 import dk.dbc.hydra.errors.WorkerErrorTypes;
@@ -25,7 +23,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -38,25 +35,16 @@ public class ErrorsAPI {
     @EJB
     RawRepoConnector rawrepo;
 
-    private final JSONBContext jsonbContext = new JSONBContext();
-
-    @Stopwatch
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     @Path(ApplicationConstants.API_ERRORS_WORKERS)
-    public Response getErrorsByWorker() {
+    @Stopwatch
+    public List<WorkerErrors> getErrorsByWorker() throws SQLException {
         LOGGER.entry();
-        String res = "";
-
         try {
-            final List<WorkerErrors> workerErrorSummaries = rawrepo.getWorkerErrors();
-            res = jsonbContext.marshall(workerErrorSummaries);
-            return Response.ok(res, MediaType.APPLICATION_JSON).build();
-        } catch (SQLException | JSONBException ex) {
-            LOGGER.error("Exception during getErrorsByWorker", ex);
-            return Response.serverError().build();
+            return rawrepo.getWorkerErrors();
         } finally {
-            LOGGER.exit(res);
+            LOGGER.exit();
         }
     }
 
@@ -74,23 +62,16 @@ public class ErrorsAPI {
         }
     }
 
-    @Stopwatch
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     @Path(ApplicationConstants.API_ERRORS_TYPE)
-    public Response getErrorsByType() {
+    @Stopwatch
+    public List<WorkerErrorTypes> getErrorsByType() throws SQLException {
         LOGGER.entry();
-        String res = "";
-
         try {
-            final List<WorkerErrorTypes> workerErrorTypes = rawrepo.getWorkerErrorTypes();
-            res = jsonbContext.marshall(workerErrorTypes);
-            return Response.ok(res, MediaType.APPLICATION_JSON).build();
-        } catch (SQLException | JSONBException ex) {
-            LOGGER.error("Exception during getErrorsByType", ex);
-            return Response.serverError().build();
+            return rawrepo.getWorkerErrorTypes();
         } finally {
-            LOGGER.exit(res);
+            LOGGER.exit();
         }
     }
 }
