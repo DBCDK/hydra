b1bc6d1170691214bc7fdcbd82d20db10276f854
Anders Thrane Michelsen <atm@dbc.dk>
2021-01-21 09:13:56 +0100

    Clean up application constants
    Auditors: jbn, jbr

--- hydra-api/src/main/java/dk/dbc/hydra/common/ApplicationConstants.java
+++ hydra-api/src/main/java/dk/dbc/hydra/common/ApplicationConstants.java
@@ -12,20 +12,12 @@ public class ApplicationConstants {
     }
 
     public static final String API_QUEUE = "/queue";
-    public static final String API_QUEUE_VALIDATE = "validate";
-    public static final String API_QUEUE_PROCESS = "process";
-    public static final String API_QUEUE_TYPES = "types";
-    public static final String API_QUEUE_PROVIDERS = "providers";
-
-    public static final String API_LIBRARY = "/library";
-    public static final String API_LIBRARY_CATALOGING_TEMPLATE_SET = "catalogingTemplateSet";
 
     public static final String API_HYDRA = "/hydra";
     public static final String API_HYDRA_STATUS = "status";
     public static final String API_HYDRA_INSTANCE_NAME = "instance";
 
     public static final String API_STATS = "/stats";
-    public static final String API_STATS_RECORDS_SUMMARY = "/recordsSummary";
     public static final String API_STATS_QUEUE_AGENCIES = "/queueByAgency";
     public static final String API_STATS_QUEUE_WORKERS = "/queueByWorker";
 
@@ -33,5 +25,4 @@ public class ApplicationConstants {
     public static final String API_ERRORS_WORKERS = "/byWorker";
     public static final String API_ERRORS_TYPE = "/byType";
 
-    public static final String INSTANCE_NAME = "INSTANCE_NAME";
 }
